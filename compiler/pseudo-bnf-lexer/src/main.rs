9d43dd5c41ff871ca140a6559d4d6a1403984afb
Tor Gunnar Houeland <887395+houeland@users.noreply.github.com>
2019-05-20 02:40:34 +0100

    Parse initial rules

--- compiler/pseudo-bnf-lexer/src/main.rs
+++ compiler/pseudo-bnf-lexer/src/main.rs
@@ -1,15 +1,55 @@
 extern crate regex;
 
 use regex::Regex;
+use std::fmt;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
 #[derive(Debug)]
+struct RuleAction {
+    action_type: String,
+    consume_token: String,
+    rule_name: String,
+    unknown_token: String,
+}
+
+impl fmt::Display for RuleAction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.action_type == "consume_token" {
+            write!(f, r#"consume: "{}""#, self.consume_token)
+        } else if self.action_type == "unimplemented" {
+            write!(f, "unimplemented: {}", self.unknown_token)
+        } else if self.action_type == "rule_name" {
+            write!(f, "sub-rule: {}", self.rule_name)
+        } else if self.action_type == "repeated rule_name with separator" {
+            write!(f, r#"repeated "{}"-separated sub-rule: {}"#, self.consume_token, self.rule_name)
+        } else {
+            write!(f, "unknown: {}", self.unknown_token)
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Rule {
     name: String,
     rule_type: String,
-    consume_token: String,
+    actions: Vec<RuleAction>,
+}
+
+impl fmt::Display for Rule {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "rule: {}", self.name)?;
+        if self.rule_type == "user-specified-token" {
+            write!(f, " read user-specified-token\n")
+        } else {
+            write!(f, " actions:\n")?;
+            for action in self.actions.iter() {
+                write!(f, "  {}\n", action)?;
+            }
+            Ok(())
+        }
+    }
 }
 
 fn parse_rule(line: &str) -> Rule {
@@ -20,25 +60,56 @@ fn parse_rule(line: &str) -> Rule {
         return Rule {
             name,
             rule_type: "user-specified-token".to_string(),
-            consume_token: "".to_string(),
+            actions: vec![],
         };
     }
     let string_pattern = Regex::new(r#"^"([^"]+)"$"#).unwrap();
-    let first = right_hand[0];
-    if string_pattern.is_match(first) {
-        return Rule {
-            name,
-            rule_type: "consume_token".to_string(),
-            consume_token: string_pattern.captures(first).unwrap()[1].to_string(),
-        };
+    let rule_pattern = Regex::new(r#"^(<[A-Z-]+>)$"#).unwrap();
+    let repeated_rule_pattern = Regex::new(r#"^(<[A-Z-]+>)\*"(.)"$"#).unwrap();
+
+    let mut actions = Vec::new();
+    for &token in right_hand.iter() {
+        if string_pattern.is_match(token) {
+            actions.push(RuleAction {
+                action_type: "consume_token".to_string(),
+                consume_token: string_pattern.captures(token).unwrap()[1].to_string(),
+                rule_name: "".to_string(),
+                unknown_token: "".to_string(),
+            });
+        } else if token == "|" {
+            actions.push(RuleAction {
+                action_type: "unimplemented".to_string(),
+                consume_token: "".to_string(),
+                rule_name: "".to_string(),
+                unknown_token: "|".to_string(),
+            });
+        } else if rule_pattern.is_match(token) {
+            actions.push(RuleAction {
+                action_type: "rule_name".to_string(),
+                consume_token: "".to_string(),
+                rule_name: rule_pattern.captures(token).unwrap()[1].to_string(),
+                unknown_token: "".to_string(),
+            });
+        } else if repeated_rule_pattern.is_match(token) {
+            actions.push(RuleAction {
+                action_type: "repeated rule_name with separator".to_string(),
+                consume_token: repeated_rule_pattern.captures(token).unwrap()[2].to_string(),
+                rule_name: repeated_rule_pattern.captures(token).unwrap()[1].to_string(),
+                unknown_token: "".to_string(),
+            });
+        } else {
+            actions.push(RuleAction {
+                action_type: "unknown".to_string(),
+                consume_token: "".to_string(),
+                rule_name: "".to_string(),
+                unknown_token: token.to_string(),
+            });
+        }
     }
-    // let rule_type = match start {
-    //     start if start.is_whitespace() => {}
-    // }
     let r = Rule {
         name,
-        rule_type: "unknown".to_string(),
-        consume_token: "".to_string(),
+        rule_type: "actions".to_string(),
+        actions,
     };
     return r;
 }
@@ -50,7 +121,7 @@ fn read_grammar_from_file(filename: &str) {
     for line in reader.lines() {
         let s = line.unwrap();
         let rule = parse_rule(&s);
-        println!("rule: {:?}", rule);
+        println!("{}", rule);
     }
 }
 
