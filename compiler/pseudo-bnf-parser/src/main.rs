dc7a1bf5027d1d42bec833174c9dfab88142a11b
Tor Gunnar Houeland <887395+houeland@users.noreply.github.com>
2019-05-26 12:54:11 +0100

    Use enum types in parser implementation

--- compiler/pseudo-bnf-parser/src/main.rs
+++ compiler/pseudo-bnf-parser/src/main.rs
@@ -9,8 +9,16 @@ use std::io::BufRead;
 use std::io::BufReader;
 
 #[derive(Debug)]
+enum RuleActionType {
+    ConsumeToken,
+    RuleName,
+    RepeatedRuleNameWithSeparator,
+    Unknown,
+}
+
+#[derive(Debug)]
 struct RuleAction {
-    action_type: String,
+    action_type: RuleActionType,
     consume_token: String,
     rule_name: String,
     unknown_token: String,
@@ -18,28 +26,30 @@ struct RuleAction {
 
 impl fmt::Display for RuleAction {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if self.action_type == "consume_token" {
-            write!(f, r#"consume: "{}""#, self.consume_token)
-        } else if self.action_type == "unimplemented" {
-            write!(f, "unimplemented: {}", self.unknown_token)
-        } else if self.action_type == "rule_name" {
-            write!(f, "sub-rule: {}", self.rule_name)
-        } else if self.action_type == "repeated rule_name with separator" {
-            write!(
+        match self.action_type {
+            RuleActionType::ConsumeToken => write!(f, r#"consume: "{}""#, self.consume_token),
+            RuleActionType::RuleName => write!(f, "sub-rule: {}", self.rule_name),
+            RuleActionType::RepeatedRuleNameWithSeparator => write!(
                 f,
                 r#"repeated "{}"-separated sub-rule: {}"#,
                 self.consume_token, self.rule_name
-            )
-        } else {
-            write!(f, "unknown: {}", self.unknown_token)
+            ),
+            RuleActionType::Unknown => write!(f, "unknown: {}", self.unknown_token),
         }
     }
 }
 
 #[derive(Debug)]
+enum RuleType {
+    UserSpecifiedName,
+    RuleChoice,
+    Actions,
+}
+
+#[derive(Debug)]
 struct Rule {
     name: String,
-    rule_type: String,
+    rule_type: RuleType,
     actions: Vec<RuleAction>,
     sub_rule_names: Vec<String>,
 }
@@ -47,22 +57,22 @@ struct Rule {
 impl fmt::Display for Rule {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "rule: {}", self.name)?;
-        if self.rule_type == "user-specified-name" {
-            write!(f, " read user-specified-name\n")
-        } else if self.rule_type == "rule-choice" {
-            write!(f, " one of the following sub-rules:\n")?;
-            for r in self.sub_rule_names.iter() {
-                write!(f, "  {}\n", r)?;
-            }
-            Ok(())
-        } else if self.rule_type == "actions" {
-            write!(f, " action sequence:\n")?;
-            for action in self.actions.iter() {
-                write!(f, "  {}\n", action)?;
-            }
-            Ok(())
-        } else {
-            write!(f, " not recognized!\n")
+        match self.rule_type {
+            RuleType::UserSpecifiedName => write!(f, " read user-specified-name\n"),
+            RuleType::RuleChoice => {
+                write!(f, " one of the following sub-rules:\n")?;
+                for r in self.sub_rule_names.iter() {
+                    write!(f, "  {}\n", r)?;
+                }
+                Ok(())
+            },
+            RuleType::Actions => {
+                write!(f, " action sequence:\n")?;
+                for action in self.actions.iter() {
+                    write!(f, "  {}\n", action)?;
+                }
+                Ok(())
+            },
         }
     }
 }
@@ -78,14 +88,14 @@ fn parse_rule(line: &str) -> Rule {
     if right_hand == ["<<USER-SPECIFIED-NAME>>"] {
         return Rule {
             name,
-            rule_type: "user-specified-name".to_string(),
+            rule_type: RuleType::UserSpecifiedName,
             actions: vec![],
             sub_rule_names: vec![],
         };
     } else if right_hand.iter().any(|&x| x == "|") {
         return Rule {
             name,
-            rule_type: "rule-choice".to_string(),
+            rule_type: RuleType::RuleChoice,
             actions: vec![],
             sub_rule_names: right_hand
                 .iter()
@@ -100,28 +110,28 @@ fn parse_rule(line: &str) -> Rule {
     for &token in right_hand.iter() {
         if string_pattern.is_match(token) {
             actions.push(RuleAction {
-                action_type: "consume_token".to_string(),
+                action_type: RuleActionType::ConsumeToken,
                 consume_token: string_pattern.captures(token).unwrap()[1].to_string(),
                 rule_name: "".to_string(),
                 unknown_token: "".to_string(),
             });
         } else if rule_pattern.is_match(token) {
             actions.push(RuleAction {
-                action_type: "rule_name".to_string(),
+                action_type: RuleActionType::RuleName,
                 consume_token: "".to_string(),
                 rule_name: rule_pattern.captures(token).unwrap()[1].to_string(),
                 unknown_token: "".to_string(),
             });
         } else if repeated_rule_pattern.is_match(token) {
             actions.push(RuleAction {
-                action_type: "repeated rule_name with separator".to_string(),
+                action_type: RuleActionType::RepeatedRuleNameWithSeparator,
                 consume_token: repeated_rule_pattern.captures(token).unwrap()[2].to_string(),
                 rule_name: repeated_rule_pattern.captures(token).unwrap()[1].to_string(),
                 unknown_token: "".to_string(),
             });
         } else {
             actions.push(RuleAction {
-                action_type: "unknown".to_string(),
+                action_type: RuleActionType::Unknown,
                 consume_token: "".to_string(),
                 rule_name: "".to_string(),
                 unknown_token: token.to_string(),
@@ -130,7 +140,7 @@ fn parse_rule(line: &str) -> Rule {
     }
     let r = Rule {
         name,
-        rule_type: "actions".to_string(),
+        rule_type: RuleType::Actions,
         actions,
         sub_rule_names: vec![],
     };
@@ -181,122 +191,131 @@ fn consume(expected: &str, tokens: &mut Vec<&str>, indent: &str) {
 
 fn check_if_can_start_rule(grammar: &Grammar, rule_name: &str, token: &str, indent: &str) -> bool {
     let rule = grammar.rules.get(rule_name).unwrap();
-    if rule.rule_type == "actions" {
-        let first_action = &rule.actions[0];
-        // println!(r#"{}try to match {}"#, indent, first_action);
-        if first_action.action_type == "consume_token" {
-            first_action.consume_token == token
-        } else {
+    match rule.rule_type {
+        RuleType::Actions => {
+            let first_action = &rule.actions[0];
+            // println!(r#"{}try to match {}"#, indent, first_action);
+            match first_action.action_type {
+                RuleActionType::ConsumeToken => first_action.consume_token == token,
+                _ => {
+                    println!(
+                        r#"{}SYNTAX ERROR: check_if_can_start_rule at "{}""#,
+                        indent, token
+                    );
+                    println!("{}reason: first action must be consuming a token", indent);
+                    false
+                }
+            }
+        },
+        RuleType::UserSpecifiedName => {
+            let user_specified_name_pattern = Regex::new(r#"^[A-Z][a-zA-Z-]*$"#).unwrap();
+            user_specified_name_pattern.is_match(token)
+        },
+        _ => {
             println!(
                 r#"{}SYNTAX ERROR: check_if_can_start_rule at "{}""#,
                 indent, token
             );
-            println!("{}reason: first action must be consuming a token", indent);
+            println!(
+                "{}reason: only action sequences and user-specified-names are supported",
+                indent
+            );
             false
         }
-    } else if rule.rule_type == "user-specified-name" {
-        let user_specified_name_pattern = Regex::new(r#"^[A-Z][a-zA-Z-]*$"#).unwrap();
-        user_specified_name_pattern.is_match(token)
-    } else {
-        println!(
-            r#"{}SYNTAX ERROR: check_if_can_start_rule at "{}""#,
-            indent, token
-        );
-        println!(
-            "{}reason: only action sequences and user-specified-names are supported",
-            indent
-        );
-        false
     }
 }
 
 fn run_action(grammar: &Grammar, action: &RuleAction, tokens: &mut Vec<&str>, indent: &str) {
     let subindent = indent.to_owned() + "  ";
-    if action.action_type == "repeated rule_name with separator" {
-        println!("{}run_action {}", indent, action);
-        run_rule(grammar, &action.rule_name, tokens, &subindent);
-        while tokens[0] == action.consume_token {
-            consume(&action.consume_token, tokens, &subindent);
-            let matches =
-                check_if_can_start_rule(grammar, &action.rule_name, tokens[0], &subindent);
-            // println!("{} - {} - {}", subindent, action.rule_name, matches);
-            if matches {
-                run_rule(grammar, &action.rule_name, tokens, &subindent);
-            } else {
-                break;
+    match action.action_type {
+        RuleActionType::RepeatedRuleNameWithSeparator => {
+            println!("{}run_action {}", indent, action);
+            run_rule(grammar, &action.rule_name, tokens, &subindent);
+            while tokens[0] == action.consume_token {
+                consume(&action.consume_token, tokens, &subindent);
+                let matches =
+                    check_if_can_start_rule(grammar, &action.rule_name, tokens[0], &subindent);
+                // println!("{} - {} - {}", subindent, action.rule_name, matches);
+                if matches {
+                    run_rule(grammar, &action.rule_name, tokens, &subindent);
+                } else {
+                    break;
+                }
             }
-        }
-    // println!(r#"{}finished loop, next token is "{}""#, indent, tokens[0]);
-    } else if action.action_type == "rule_name" {
-        println!("{}run_action {}", indent, action);
-        run_rule(grammar, &action.rule_name, tokens, &subindent);
-    } else if action.action_type == "consume_token" {
-        println!("{}run_action {}", indent, action);
-        consume(&action.consume_token, tokens, &subindent);
-    } else {
-        println!("{}UNIMPLEMENTED ACTION! {}", indent, action);
+            // println!(r#"{}finished loop, next token is "{}""#, indent, tokens[0]);
+        },
+        RuleActionType::RuleName => {
+            println!("{}run_action {}", indent, action);
+            run_rule(grammar, &action.rule_name, tokens, &subindent);
+        },
+        RuleActionType::ConsumeToken => {
+            println!("{}run_action {}", indent, action);
+            consume(&action.consume_token, tokens, &subindent);
+        },
+        RuleActionType::Unknown => {
+            println!("{}UNKNOWN ACTION! {}", indent, action);
+        },
     }
 }
 
 fn run_rule(grammar: &Grammar, rule_name: &str, tokens: &mut Vec<&str>, indent: &str) {
     let subindent = indent.to_owned() + "  ";
     let rule = grammar.rules.get(rule_name).unwrap();
-    if rule.rule_type == "actions" {
-        println!("{}Running rule {} action sequence!", indent, rule.name);
-        for action in rule.actions.iter() {
-            run_action(grammar, action, tokens, &subindent);
-        }
-        println!("{}Rule {} done", indent, rule.name);
-    } else if rule.rule_type == "user-specified-name" {
-        println!(
-            r#"{}Running rule {} read user specified name: "{}""#,
-            indent, rule.name, tokens[0]
-        );
-        let user_specified_name_pattern = Regex::new(r#"^[A-Z][a-zA-Z-]*$"#).unwrap();
-        if user_specified_name_pattern.is_match(tokens[0]) {
-            tokens.remove(0);
-        } else {
-            println!(
-                r#"{}SYNTAX ERROR: invalid user-specified-name at "{}""#,
-                indent, tokens[0]
-            );
-        }
-    // println!(r#"{}read name: "{}""#, subindent, tokens[0]);
-    // println!(r#"{}next is: "{}""#, subindent, tokens[0]);
-    } else if rule.rule_type == "rule-choice" {
-        println!(
-            "{}Running rule {} rule-choice options {:?}:",
-            indent, rule.name, rule.sub_rule_names
-        );
-        let mut count_matches = 0;
-        let mut match_rule_name = "";
-        for rn in rule.sub_rule_names.iter() {
-            let matches = check_if_can_start_rule(grammar, rn, tokens[0], &subindent);
-            // println!("{} - {} - {}", subindent, rn, matches);
-            if matches {
-                count_matches += 1;
-                match_rule_name = rn;
+    match rule.rule_type {
+        RuleType::Actions => {
+            println!("{}Running rule {} action sequence!", indent, rule.name);
+            for action in rule.actions.iter() {
+                run_action(grammar, action, tokens, &subindent);
             }
-        }
-        if count_matches == 1 {
-            println!("{}matched rule {}!", indent, match_rule_name);
-            run_rule(grammar, match_rule_name, tokens, &subindent);
-        } else {
-            println!("{}rule-choice syntax error", indent);
+            println!("{}Rule {} done", indent, rule.name);
+        },
+        RuleType::UserSpecifiedName => {
             println!(
-                "{}reason: must match exactly 1 sub-rule, but matched {}",
-                indent, count_matches
+                r#"{}Running rule {} read user specified name: "{}""#,
+                indent, rule.name, tokens[0]
             );
-        }
-    } else {
-        println!(
-            "{}Running rule {} UNIMPLEMENTED RULE TYPE: {}",
-            indent, rule.name, rule.rule_type
-        );
+            let user_specified_name_pattern = Regex::new(r#"^[A-Z][a-zA-Z-]*$"#).unwrap();
+            if user_specified_name_pattern.is_match(tokens[0]) {
+                tokens.remove(0);
+            } else {
+                println!(
+                    r#"{}SYNTAX ERROR: invalid user-specified-name at "{}""#,
+                    indent, tokens[0]
+                );
+            }
+            // println!(r#"{}read name: "{}""#, subindent, tokens[0]);
+            // println!(r#"{}next is: "{}""#, subindent, tokens[0]);
+        },
+        RuleType::RuleChoice => {
+            println!(
+                "{}Running rule {} rule-choice options {:?}:",
+                indent, rule.name, rule.sub_rule_names
+            );
+            let mut count_matches = 0;
+            let mut match_rule_name = "";
+            for rn in rule.sub_rule_names.iter() {
+                let matches = check_if_can_start_rule(grammar, rn, tokens[0], &subindent);
+                // println!("{} - {} - {}", subindent, rn, matches);
+                if matches {
+                    count_matches += 1;
+                    match_rule_name = rn;
+                }
+            }
+            if count_matches == 1 {
+                println!("{}matched rule {}!", indent, match_rule_name);
+                run_rule(grammar, match_rule_name, tokens, &subindent);
+            } else {
+                println!("{}rule-choice syntax error", indent);
+                println!(
+                    "{}reason: must match exactly 1 sub-rule, but matched {}",
+                    indent, count_matches
+                );
+            }
+        },
     }
 }
 
-fn lex_program(grammar: &Grammar, source: &str) {
+fn parse_program(grammar: &Grammar, source: &str) {
     // let mut keys: Vec<&String> = grammar.rules.keys().collect();
     // keys.sort();
     // println!("== RULE LIST ==\n{:#?}\n", keys);
@@ -318,5 +337,5 @@ fn main() {
     let program =
         fs::read_to_string("spec/test-example-structures.torpel").expect("Could not open program");
 
-    lex_program(&grammar, &program);
+    parse_program(&grammar, &program);
 }
